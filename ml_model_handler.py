63991e92e06c0f1bcdb1b75622724b70550f3220
emilija227 <emilijalazic22@gmail.com>
2025-11-17 23:34:55 +0100

    Implementirana MLModelHandler klasa za YOLOv8 model za detekciju nasilja

--- ml_model_handler.py
+++ ml_model_handler.py
@@ -11,32 +11,36 @@ class MLModelHandler:
         self.violence_class_id = None
 
         try:
-            self.model = YOLO(model_path)
+            self.model = YOLO(model_path) #Ucitavam preuzeti model
             print("YOLOv8 model uspjeno ocitan")
 
-            self.violence_class_id = 1
+            self.violence_class_id = 1 #Postavljam ID klase za detekciju na 1, jer je to u ovom modelu violence
 
         except FileNotFoundError:
             print("GRESKA: Fajl modela nije pronadjen")
         except Exception as e:
             print(f"GRESKA pri ucitavanju YOLO modela: {e}")
 
-    def detect_violence(self, frame_array):
+    def detect_violence(self, frame_array): #Prima 1 frejm videa i vraca listu detekcija nasilja
         if self.model is None or frame_array is None:
             return []
 
-        results = self.model.predict(frame_array, conf=0.5, iou=0.5, verbose=False)
+        results = self.model.predict(frame_array, conf=0.5, iou=0.5, verbose=False) #Pokrecem YOLO algoritam na datom frejmu
+        #Prag detekcije 50%
+        #iou rjesava problem detekcije istog objekta vise puta
+        #verbose na False da YOLO ne bi ispisivao tehnicke poruke
 
         violence_detections = []
 
-        if results and results[0].boxes:
-            boxes = results[0].boxes
+        if results and results[0].boxes: #Da li je predict() vratio uopste rezultate i da li oni sadrze detektovane kutije
+            boxes = results[0].boxes #Objekat koji sadrzi sve koordinate, klase i skorove za frejm
 
+            #Iteriramo svaki put kada pronadjemo detekciju
             for i in range(len(boxes.cls)):
                 class_id = int(boxes.cls[i].item())
 
-                if class_id == self.violence_class_id:
-                    box_coords = boxes.xyxy[i].cpu().numpy().astype(int)
+                if class_id == self.violence_class_id: #Ako je klasa koja je detektovana = 1 tj 'violence'
+                    box_coords = boxes.xyxy[i].cpu().numpy().astype(int) #Izvlacenje koordinata
                     confidence = boxes.conf[i].item()
 
                     violence_detections.append({
@@ -45,23 +49,3 @@ class MLModelHandler:
                     })
 
         return violence_detections
-
-
-# Blok za testiranje
-if __name__ == '__main__':
-    # 1. Inicijalizacija handlera
-    ml_handler = MLModelHandler()
-
-    # 2. Testiranje (potreban nam je dummy frejm)
-    if ml_handler.model:
-        print("\n--- TEST YOLO DETEKCIJE ---")
-
-        # Kreiramo lažni frejm 640x480 (YOLO optimalna ulazna veličina)
-        dummy_frame = np.zeros((480, 640, 3), dtype=np.uint8)
-
-        # Testiramo da li funkcija radi
-        detections = ml_handler.detect_violence(dummy_frame)
-
-        # Ako je sve ispravno instalirano, ovo treba da radi bez grešaka
-        print(f"Simulirana detekcija na praznom frejmu. Rezultat (lista detekcija): {detections}")
-        print("Model je uspešno obradio frejm.")
