667b287bcc955ab1fdc919c33c7b8e85836ff69b
emilija227 <emilijalazic22@gmail.com>
2025-11-22 22:19:47 +0100

    Ucitava video, klizni prozor

--- video_handler.py
+++ video_handler.py
@@ -1,58 +1,42 @@
 import cv2
 import numpy as np
 
-
 class VideoHandler:
     # Postavljamo clip_size i target_res da odgovaraju modelu
     CLIP_SIZE = 64
     TARGET_RES = (224, 224)
 
     def __init__(self, video_path):
-        """Inicijalizuje video čitač i postavlja parametre klipa."""
-        self.cap = cv2.VideoCapture(video_path)
+        self.cap = cv2.VideoCapture(video_path)  # Inicijalizuje video čitač i postavlja parametre klipa.
         self.clip = []  # Lista za čuvanje tekuće sekvence frejmova (Sliding Window)
 
         if not self.cap.isOpened():
             print("Greška: Nije moguće otvoriti video fajl. Proverite putanju i FFmpeg.")
 
     def read_single_frame(self):
-        """Čita jedan frejm iz videa i obrađuje ga."""
-        ret, frame = self.cap.read()
+        ret, frame = self.cap.read()   #Čita jedan frejm iz videa i obrađuje ga.
 
         if not ret:
-            # Kraj videa ili greška u čitanju
             return None, None
 
-            # Promena veličine (resize) frejma na 224x224 (za model)
+        # Promena veličine (resize) frejma na 224x224 (za model)
         resized_frame = cv2.resize(frame, self.TARGET_RES, interpolation=cv2.INTER_AREA)
 
-        # Vraćamo obrađeni frejm (za model) i originalni frejm (za prikaz)
-        return resized_frame, frame
+        return resized_frame, frame  # Vraćamo obrađeni frejm (za model) i originalni frejm (za prikaz)
+
 
     def get_sliding_window(self, processed_frame):
-        """
-        Dodaje novi frejm u klizni prozor i vraća klip za predikciju.
-
-        :param processed_frame: Frejm dimenzija (224, 224, 3)
-        :return: Numpy array klipa (64, 224, 224, 3) ili None
-        """
-        # Dodavanje novog frejma u listu klipova
-        self.clip.append(processed_frame)
+        self.clip.append(processed_frame) # Dodavanje novog frejma u listu klipova
 
         if len(self.clip) < self.CLIP_SIZE:
-            # Klip još nije pun (prvih 64 frejma)
-            return None
+            return None # Klip još nije pun (prvih 64 frejma)
 
         if len(self.clip) > self.CLIP_SIZE:
-            # Ako je klip prepun (Sliding Window logika), izbacujemo najstariji frejm
-            self.clip.pop(0)
+            self.clip.pop(0) # Ako je klip prepun (Sliding Window logika), izbacujemo najstariji frejm
 
-        # Klip je tačno 64 frejma, pretvaramo ga u tenzor za model
-        clip_array = np.array(self.clip)
+        clip_array = np.array(self.clip) # Klip je tačno 64 frejma, pretvaramo ga u tenzor za model
 
-        # Vraćamo tenzor spreman za predikciju
         return clip_array
 
     def release(self):
-        """Oslobađa resurs kamere/videa."""
-        self.cap.release()
+        self.cap.release() #Oslobadjanje resursa
